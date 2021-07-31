9135a641d2fab2634776d71207b55f42ecf8f34a
E2-waite <Edward2.waite@gmail.com>
2021-08-01 00:16:20 +0100

    Renamed project to 'Shadowvale' and added title/icons to game

---
+++ Shadowvale/Assets/Scripts/UI/BuildingMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BuildingMenu : MonoBehaviour, IPointerClickHandler
+{
+    GameObject[] buttons;
+    public GameObject button;
+    public Sprite cancelIcon;
+    private void Start()
+    {
+        Spawner spawner = Spawner.Instance;
+        buttons = new GameObject[spawner.buildings.Count + 1];
+        Vector2 pos = new Vector2(Screen.width - 25, 25);
+        for (int i = 0; i < spawner.buildings.Count; i++)
+        {
+            buttons[i] = Instantiate(button, pos, Quaternion.identity);
+            buttons[i].transform.SetParent(transform);
+            buttons[i].GetComponent<Image>().sprite = spawner.buildings[i].sprite;
+            pos.x -= 25;
+        }
+        buttons[spawner.buildings.Count] = Instantiate(button, pos, Quaternion.identity);
+        buttons[spawner.buildings.Count].transform.SetParent(transform);
+        buttons[spawner.buildings.Count].GetComponent<RectTransform>().sizeDelta = new Vector3(25, 25, 0);
+        buttons[spawner.buildings.Count].GetComponent<Image>().sprite = cancelIcon;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.pointerCurrentRaycast.gameObject == buttons[Spawner.Instance.buildings.Count])
+        {
+            GameController.Instance.gameState = GameController.GameState.destroy;
+            return;
+        }
+        
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (eventData.pointerCurrentRaycast.gameObject == buttons[i])
+            {
+                Spawner.Instance.selectedTemplate = i;
+                GameController.Instance.gameState = GameController.GameState.build;
+                return;
+            }
+        }
+    }
+}
---
+++ Shadowvale/Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    enum Buttons
+    {
+        play,
+        settings,
+        exit
+    }
+
+    public enum Menu
+    {
+        main,
+        saves,
+        settings
+    }
+
+    public Menu currentMenu = Menu.main;
+    public GameObject[] menus;
+    public GameObject[] menuButtons = new GameObject[3];
+    public GameObject[] gameSaves = new GameObject[3];
+    public Color textColour, rolloverColour;
+    private void Start()
+    {
+        for (int i = 0; i < gameSaves.Length; i++)
+        {
+            if (System.IO.File.Exists(Application.persistentDataPath + "/SaveData" + (i + 1).ToString() + ".json"))
+            {
+                gameSaves[i].transform.GetChild(0).GetComponent<Text>().text = ("-Save " + (i + 1).ToString() + "-");
+            }
+        }
+        menus[1].SetActive(false);
+        menus[2].SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (currentMenu == Menu.main)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SwitchMenu(Menu.main);
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (currentMenu == Menu.main)
+        {
+            if (eventData.pointerCurrentRaycast.gameObject == menuButtons[(int)Buttons.play])
+            {
+                SwitchMenu(Menu.saves);
+            }
+            else if (eventData.pointerCurrentRaycast.gameObject == menuButtons[(int)Buttons.settings])
+            {
+                SwitchMenu(Menu.settings);
+            }
+            else if (eventData.pointerCurrentRaycast.gameObject == menuButtons[(int)Buttons.exit])
+            {
+                Application.Quit();
+            }
+        }
+        else if (currentMenu == Menu.saves)
+        {
+            for (int i = 0; i < gameSaves.Length; i++)
+            {
+                if (eventData.pointerCurrentRaycast.gameObject == gameSaves[i].transform.GetChild(1))
+                {
+                    File.Delete(Application.persistentDataPath + "/SaveData" + (i + 1).ToString() + ".json");
+                    gameSaves[i].transform.GetChild(0).GetComponent<Text>().text = "-Blank Save";
+                    gameSaves[i].transform.GetChild(1).gameObject.SetActive(false);
+
+                }
+                else if (eventData.pointerCurrentRaycast.gameObject == gameSaves[i])
+                {
+                    Save.file = "SaveData" + (i + 1).ToString();
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+
+    }
+
+    void SwitchMenu(Menu menu)
+    {
+        menus[(int)currentMenu].SetActive(false);
+        currentMenu = menu;
+        menus[(int)currentMenu].SetActive(true);
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if (pointerEventData.pointerCurrentRaycast.gameObject != null && pointerEventData.pointerCurrentRaycast.gameObject.CompareTag("Button"))
+        {
+            pointerEventData.pointerCurrentRaycast.gameObject.transform.GetChild(0).GetComponent<Text>().color = rolloverColour;
+        }
+        if (currentMenu == Menu.saves)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (pointerEventData.pointerCurrentRaycast.gameObject == gameSaves[i] && System.IO.File.Exists(Application.persistentDataPath + "/SaveData" + (i + 1).ToString() + ".json"))
+                {
+                    gameSaves[i].transform.GetChild(1).gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            menuButtons[i].transform.GetChild(0).GetComponent<Text>().color = textColour;
+            gameSaves[i].transform.GetChild(0).GetComponent<Text>().color = textColour;
+            gameSaves[i].transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}
